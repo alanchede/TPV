c3df6c0fa70b017f05b7ad8b3a11796b0fb8ad9c
Biblioteca <Biblioteca@DESKTOP-O8QK4OQ>
2022-02-07 16:56:02 +0100

    creamos variable

--- GIT/src/Programa.java
+++ GIT/src/Programa.java
@@ -14,6 +14,9 @@ public class Programa {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int variable;
+        variable = 3;
+        
     }
     
 }
